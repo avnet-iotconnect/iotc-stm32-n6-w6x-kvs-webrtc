0b69f9aa91c1302d49ddedeac82de5d7c718bae6
mlamp99 <michael.lamp@avnet.com>
2026-07-30 12:42:30 -0500

    Build: sync generated makefiles/objects with current .cproject/.project
    Regenerated by the headless build; includes stm32n6xx_hal_ltdc.o per
    the committed .project linked-resource fix.

--- Appli/HW_Crypto/Common/net/W6X_ARCH_T02/subdir.mk
+++ Appli/HW_Crypto/Common/net/W6X_ARCH_T02/subdir.mk
@@ -7,28 +7,28 @@
 C_SRCS += \
 ../Common/net/W6X_ARCH_T02/dhcp_server_raw.c \
 ../Common/net/W6X_ARCH_T02/eth_netif.c \
-../Common/net/W6X_ARCH_T02/rtl8211.c \
 ../Common/net/W6X_ARCH_T02/lwip.c \
 ../Common/net/W6X_ARCH_T02/lwip_netif.c \
 ../Common/net/W6X_ARCH_T02/net_diag.c \
+../Common/net/W6X_ARCH_T02/rtl8211.c \
 ../Common/net/W6X_ARCH_T02/st67w6x_netconn.c 
 
 OBJS += \
 ./Common/net/W6X_ARCH_T02/dhcp_server_raw.o \
 ./Common/net/W6X_ARCH_T02/eth_netif.o \
-./Common/net/W6X_ARCH_T02/rtl8211.o \
 ./Common/net/W6X_ARCH_T02/lwip.o \
 ./Common/net/W6X_ARCH_T02/lwip_netif.o \
 ./Common/net/W6X_ARCH_T02/net_diag.o \
+./Common/net/W6X_ARCH_T02/rtl8211.o \
 ./Common/net/W6X_ARCH_T02/st67w6x_netconn.o 
 
 C_DEPS += \
 ./Common/net/W6X_ARCH_T02/dhcp_server_raw.d \
 ./Common/net/W6X_ARCH_T02/eth_netif.d \
-./Common/net/W6X_ARCH_T02/rtl8211.d \
 ./Common/net/W6X_ARCH_T02/lwip.d \
 ./Common/net/W6X_ARCH_T02/lwip_netif.d \
 ./Common/net/W6X_ARCH_T02/net_diag.d \
+./Common/net/W6X_ARCH_T02/rtl8211.d \
 ./Common/net/W6X_ARCH_T02/st67w6x_netconn.d 
 
 
@@ -39,7 +39,7 @@ Common/net/W6X_ARCH_T02/%.o Common/net/W6X_ARCH_T02/%.su Common/net/W6X_ARCH_T02
 clean: clean-Common-2f-net-2f-W6X_ARCH_T02
 
 clean-Common-2f-net-2f-W6X_ARCH_T02:
-	-$(RM) ./Common/net/W6X_ARCH_T02/dhcp_server_raw.cyclo ./Common/net/W6X_ARCH_T02/dhcp_server_raw.d ./Common/net/W6X_ARCH_T02/dhcp_server_raw.o ./Common/net/W6X_ARCH_T02/dhcp_server_raw.su ./Common/net/W6X_ARCH_T02/lwip.cyclo ./Common/net/W6X_ARCH_T02/lwip.d ./Common/net/W6X_ARCH_T02/lwip.o ./Common/net/W6X_ARCH_T02/lwip.su ./Common/net/W6X_ARCH_T02/lwip_netif.cyclo ./Common/net/W6X_ARCH_T02/lwip_netif.d ./Common/net/W6X_ARCH_T02/lwip_netif.o ./Common/net/W6X_ARCH_T02/lwip_netif.su ./Common/net/W6X_ARCH_T02/net_diag.cyclo ./Common/net/W6X_ARCH_T02/net_diag.d ./Common/net/W6X_ARCH_T02/net_diag.o ./Common/net/W6X_ARCH_T02/net_diag.su ./Common/net/W6X_ARCH_T02/st67w6x_netconn.cyclo ./Common/net/W6X_ARCH_T02/st67w6x_netconn.d ./Common/net/W6X_ARCH_T02/st67w6x_netconn.o ./Common/net/W6X_ARCH_T02/st67w6x_netconn.su
+	-$(RM) ./Common/net/W6X_ARCH_T02/dhcp_server_raw.cyclo ./Common/net/W6X_ARCH_T02/dhcp_server_raw.d ./Common/net/W6X_ARCH_T02/dhcp_server_raw.o ./Common/net/W6X_ARCH_T02/dhcp_server_raw.su ./Common/net/W6X_ARCH_T02/eth_netif.cyclo ./Common/net/W6X_ARCH_T02/eth_netif.d ./Common/net/W6X_ARCH_T02/eth_netif.o ./Common/net/W6X_ARCH_T02/eth_netif.su ./Common/net/W6X_ARCH_T02/lwip.cyclo ./Common/net/W6X_ARCH_T02/lwip.d ./Common/net/W6X_ARCH_T02/lwip.o ./Common/net/W6X_ARCH_T02/lwip.su ./Common/net/W6X_ARCH_T02/lwip_netif.cyclo ./Common/net/W6X_ARCH_T02/lwip_netif.d ./Common/net/W6X_ARCH_T02/lwip_netif.o ./Common/net/W6X_ARCH_T02/lwip_netif.su ./Common/net/W6X_ARCH_T02/net_diag.cyclo ./Common/net/W6X_ARCH_T02/net_diag.d ./Common/net/W6X_ARCH_T02/net_diag.o ./Common/net/W6X_ARCH_T02/net_diag.su ./Common/net/W6X_ARCH_T02/rtl8211.cyclo ./Common/net/W6X_ARCH_T02/rtl8211.d ./Common/net/W6X_ARCH_T02/rtl8211.o ./Common/net/W6X_ARCH_T02/rtl8211.su ./Common/net/W6X_ARCH_T02/st67w6x_netconn.cyclo ./Common/net/W6X_ARCH_T02/st67w6x_netconn.d ./Common/net/W6X_ARCH_T02/st67w6x_netconn.o ./Common/net/W6X_ARCH_T02/st67w6x_netconn.su
 
 .PHONY: clean-Common-2f-net-2f-W6X_ARCH_T02
 
